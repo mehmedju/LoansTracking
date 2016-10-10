dd6fb2b24494677806cc7d5be93ce79afd742ebc
irhad ljubcic <irhadlj@maestralsolutions.com>
2016-10-10 11:37:51 +0200

    add DB

---
+++ LoansTracking/LoansDbs/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using LoansDbs.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoansDbs
+{
+    public class AppContext: DbContext
+    {
+        public AppContext() : base("LoansContext") { }
+
+        public DbSet<Loan> Loans { get; set; }
+    }
+}
