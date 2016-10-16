225c526926cb8b094080d25b787738164786e204
martina-vistica <martina@gigischool.com>
2016-10-16 19:49:10 +0200

    updated profile screen

--- LoansTracking.UI/LoansTracking.UI/Scripts/angular-app/js/profile/profileController.js
+++ LoansTracking.UI/LoansTracking.UI/Scripts/angular-app/js/profile/profileController.js
@@ -2,5 +2,16 @@
     .controller('profileController', ['$scope', function ($scope) {
         'use strict';
 
+        $scope.user = {
+            firstName: "Angular",
+            lastName: "User",
+            dateOfBirth: Date.now(),
+            mobileNumber: "033 987 654",
+            address: "Milana Preloga 12",
+            location: "Sarajevo, BiH",
+            position: "Software developer",
+            company: "Mistral"
+        }
     }
-    ]));
+
+]));
