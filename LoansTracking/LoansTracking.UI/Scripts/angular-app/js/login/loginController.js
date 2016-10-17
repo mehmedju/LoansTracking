fbc57f130040d2cc59d5604ad201275216c02f79
mehmedju <mehmedju@maestralsolutions.com>
2016-10-17 12:34:02 +0200

    Login + loans screen updated

--- LoansTracking/LoansTracking.UI/Scripts/angular-app/js/login/loginController.js
+++ LoansTracking/LoansTracking.UI/Scripts/angular-app/js/login/loginController.js
@@ -2,8 +2,20 @@
     .controller('loginController', ['$scope', '$location', function ($scope, $location) {
         'use strict';
 
+        angular.extend($scope, {
+            showErrorMessage: false,
+            user: {},
+            temporaryUsername: "admin",
+            temporaryPassword: "password"
+        });
+
         $scope.goToSignup = function () {
             $location.path('/signup');
+        }
+
+        $scope.login = function (username, password) {
+            (username === $scope.temporaryUsername && password === $scope.temporaryPassword) ?
+                $location.path('/loans') : $scope.showErrorMessage = true;
         }
     }
     ]));
