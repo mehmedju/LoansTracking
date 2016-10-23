a2fe048ba5f20ec924df0f8195f67b36877f3e91
Haris Brackovic <harisbr@maestralsolutions.com>
2016-10-23 22:07:07 +0200

    create account for person API

--- LoansTracking/LoansTracking.DB/Migrations/Configuration.cs
+++ LoansTracking/LoansTracking.DB/Migrations/Configuration.cs
@@ -16,39 +16,39 @@ namespace LoansTracking.DB.Migrations
         protected override void Seed(AppContext context)
         {
 
-            context.People.AddOrUpdate(
-              p => p.Id,
-              new Person { FirstName = "John", LastName = "Doe" },
-              new Person { FirstName = "Johane", LastName = "Doe" }
-            );
-            context.SaveChanges();
-            context.Loans.AddOrUpdate(
-             l => l.Id,
-             new Loan { Amount = 500, DueDate = DateTime.Now.AddMonths(6), PaidOff = true, Person = context.People.Find(1) },
-             new Loan { Amount = 2000, DueDate = DateTime.Now.AddMonths(8), PaidOff = false, Person = context.People.Find(2) }
-            );
-            context.SaveChanges();
-            context.Payments.AddOrUpdate(
-            x => x.Id,
-             new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
-             new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
-             new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
-             new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
-             new Payment { AmountPaid = 70, Date = DateTime.Now, Loan = context.Loans.Find(1) },
-             new Payment { AmountPaid = 30, Date = DateTime.Now, Loan = context.Loans.Find(1) },
-             new Payment { AmountPaid = 150, Date = DateTime.Now, Loan = context.Loans.Find(2) },
-             new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(2) },
-             new Payment { AmountPaid = 190, Date = DateTime.Now, Loan = context.Loans.Find(2) },
-             new Payment { AmountPaid = 300, Date = DateTime.Now, Loan = context.Loans.Find(2) },
-             new Payment { AmountPaid = 70, Date = DateTime.Now, Loan = context.Loans.Find(2) },
-             new Payment { AmountPaid = 30, Date = DateTime.Now, Loan = context.Loans.Find(2) },
-             new Payment { AmountPaid = 10, Date = DateTime.Now, Loan = context.Loans.Find(2) }
-            );
+            //context.People.AddOrUpdate(
+            //  p => p.Id,
+            //  new Person { FirstName = "John", LastName = "Doe" },
+            //  new Person { FirstName = "Johane", LastName = "Doe" }
+            //);
+            //context.SaveChanges();
+            //context.Loans.AddOrUpdate(
+            // l => l.Id,
+            // new Loan { Amount = 500, DueDate = DateTime.Now.AddMonths(6), PaidOff = true, Person = context.People.Find(1) },
+            // new Loan { Amount = 2000, DueDate = DateTime.Now.AddMonths(8), PaidOff = false, Person = context.People.Find(2) }
+            //);
+            //context.SaveChanges();
+            //context.Payments.AddOrUpdate(
+            //x => x.Id,
+            // new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
+            // new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
+            // new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
+            // new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(1) },
+            // new Payment { AmountPaid = 70, Date = DateTime.Now, Loan = context.Loans.Find(1) },
+            // new Payment { AmountPaid = 30, Date = DateTime.Now, Loan = context.Loans.Find(1) },
+            // new Payment { AmountPaid = 150, Date = DateTime.Now, Loan = context.Loans.Find(2) },
+            // new Payment { AmountPaid = 100, Date = DateTime.Now, Loan = context.Loans.Find(2) },
+            // new Payment { AmountPaid = 190, Date = DateTime.Now, Loan = context.Loans.Find(2) },
+            // new Payment { AmountPaid = 300, Date = DateTime.Now, Loan = context.Loans.Find(2) },
+            // new Payment { AmountPaid = 70, Date = DateTime.Now, Loan = context.Loans.Find(2) },
+            // new Payment { AmountPaid = 30, Date = DateTime.Now, Loan = context.Loans.Find(2) },
+            // new Payment { AmountPaid = 10, Date = DateTime.Now, Loan = context.Loans.Find(2) }
+            //);
 
-            context.Notes.AddOrUpdate(
-                n => n.Id,
-                new Note {Text = "This is the first Note", Title = "First Note"},
-                new Note {Text = "And this is the second one", Title = "Second Note"});
+            //context.Notes.AddOrUpdate(
+            //    n => n.Id,
+            //    new Note {Text = "This is the first Note", Title = "First Note"},
+            //    new Note {Text = "And this is the second one", Title = "Second Note"});
         }
 }
 }
---
+++ LoansTracking/LoansTracking.WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,92 @@
+﻿using LoansTracking.DB.DataAccess;
+using LoansTracking.DB.Entities;
+using LoansTracking.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace LoansTracking.WebApi.Controllers
+{
+    public class AccountsController : BaseController<Account>
+    {
+        public AccountsController(Repository<Account> depo) : base(depo) { }
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(Repository.Get().ToList().Select(x => Factory.Create(x)).ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Post(AccountModel model)
+        {
+            try
+            {
+                Repository<Person> personRepo = new Repository<Person>(Repository.BaseContext());
+                Person person = personRepo.Get().Where(x => x.Id == model.Person).FirstOrDefault();
+                if (person != null)
+                {
+                    Account account = Parser.Create(model, Repository.BaseContext());
+                    Repository.Insert(account);
+                    return Ok(Factory.Create(account));
+                }
+                else
+                    return BadRequest("There is no such person in the database");
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Put(int id, AccountModel model)
+        {
+            try
+            {
+                Account account = Repository.Get(id);
+                if (account == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    Repository.Update(Parser.Create(model, Repository.BaseContext()), account.Id);
+                    return Ok(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                Account account = Repository.Get(id);
+                if (account == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    Repository.Delete(id);
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
