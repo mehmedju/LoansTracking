2965355ec0693d5296b838fc3ed0440a23692ba3
martina-vistica <martina@gigischool.com>
2016-10-29 12:48:18 +0200

    changes

--- LoansTracking/LoansTracking.UI/Scripts/angular-app/js/profile/profileController.js
+++ LoansTracking/LoansTracking.UI/Scripts/angular-app/js/profile/profileController.js
@@ -1,20 +1,40 @@
 ﻿(angular.module('app')
-    .controller('profileController', ['$scope', function ($scope) {
+    .controller('profileController', ['$scope', 'profileService', function ($scope, profileService) {
         'use strict';
 
-        $scope.user = {
-            firstName: "Angular",
-            lastName: "User",
-            dateOfBirth: Date.now(),
-            gender: "male",
-            mobileNumber: "033 987 654",
-            address: "Milana Preloga 12",
-            location: "Sarajevo, BiH",
-            occupation: "Software developer",
-            company: "Mistral",
-            email: "angular@mistral.com",
-            username: "Mistralko"
-        }
+        angular.extend($scope, {
+            user : {
+                id: 3,
+                firstName: "Angular",
+                lastName: "User",
+                dateOfBirth: new Date(),
+                gender: "Male",
+                mobileNumber: "033 987 654",
+                address: "Milana Preloga 12",
+                location: "Sarajevo, BiH",
+                occupation: "Software developer",
+                company: "Mistral",
+                email: "angular@mistral.com",
+                password: ""
+            },
+            birthDate: {
+                opened: false
+            }
+        });             
+
+        profileService.getLoggedUser(5).then(function (data) {
+            $scope.loggedUser = data;
+            console.log($scope.loggedUser);
+        });
+
+        $scope.openBirthDate = function () {
+            $scope.birthDate.opened = true;
+        };
+
+        $scope.updateProfile = function (user, id) {
+            console.log(user, id);
+            profileService.updateProfile(user, id);
+        };
     }
 
 ]));
--- LoansTracking/LoansTracking.UI/Scripts/config/config.js
+++ LoansTracking/LoansTracking.UI/Scripts/config/config.js
@@ -3,3 +3,6 @@
 var loansUrl = baseRoute + 'loans';
 var totalStatisticsUrl = baseRoute + 'statistics/total';
 var paidOffStatitsticsUrl = baseRoute + 'statistics/paidoff';
+
+//people
+var peopleUrl = baseRoute + 'people/';
