7bae84ed1036a8efa9f5bf672ecc8d4fa43bd44c
mehmedju <mehmedju@maestralsolutions.com>
2016-10-30 23:58:28 +0100

    Fixed some things

--- LoansTracking/LoansTracking.UI/Scripts/config/config.js
+++ LoansTracking/LoansTracking.UI/Scripts/config/config.js
@@ -1,6 +1,6 @@
 ﻿var baseRoute = "http://localhost:50743/api/";
 
-var loansUrl = baseRoute + 'loans';
+var loansUrl = baseRoute + 'loans/';
 var totalStatisticsUrl = baseRoute + 'statistics/total';
 var paidOffStatitsticsUrl = baseRoute + 'statistics/paidoff';
 
