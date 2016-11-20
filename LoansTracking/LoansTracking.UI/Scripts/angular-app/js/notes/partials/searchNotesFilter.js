0e08809193ea65e98aea48d216fd82c1663cdb84
elma-ramusovic <elma@gigischool.com>
2016-11-20 23:47:08 +0100

    search notes custom filter

---
+++ LoansTracking/LoansTracking.UI/Scripts/angular-app/js/notes/partials/searchNotesFilter.js
@@ -0,0 +1,18 @@
+﻿app.filter('searchNotesFilter', function () {
+    return function (notes, searchInput) {
+        var filtered = [];
+        if (notes != undefined) {
+            angular.forEach(notes, function (note) {
+                if (note.title.substring().match(searchInput)) {
+                    filtered.push(note);
+                }
+            });
+            return filtered;
+        }
+        if (searchInput == undefined || searchInput == '')
+            return notes;
+    };
+}
+);
+
+
