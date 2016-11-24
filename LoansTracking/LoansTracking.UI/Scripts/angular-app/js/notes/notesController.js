9f548796136e4702991eb57ad9a7c01437f93cec
Irma Karasoftić <ikarasofti1@etf.unsa.ba>
2016-11-25 00:00:24 +0100

    worked on notes service

--- LoansTracking/LoansTracking.UI/Scripts/angular-app/js/notes/notesController.js
+++ LoansTracking/LoansTracking.UI/Scripts/angular-app/js/notes/notesController.js
@@ -15,6 +15,60 @@
             $scope.addMode = true;
         }
 
+        $scope.editOnId;
+        $scope.removeOnId;
+        $scope.newNote = {
+            id: null,
+            title: "",
+            text: "",
+        }
+        $scope.updateNote = function (note) {
+            notesService.updateNote(note, note.id);
+            notesService.getNotes();
+        };
+
+        $scope.removeNote = function (note) {
+            notesService.deleteNote(note.id);
+            notesService.getNotes();
+        };
+
+        $scope.editOn = function (note) {
+            $scope.removeOff();
+        }
+
+        $scope.editOn = function (note) {
+            $scope.removeOff();
+            $scope.editOnId = note.id;
+            $scope.newNote = note;
+        }
+
+        $scope.editOff = function () {
+            $scope.editOnId = null;
+            $scope.newNote = null;
+        }
+
+        $scope.checkEdit = function (note) {
+            return $scope.editOnId === note.id;
+        }
+
+        $scope.removeOn = function (note) {
+            $scope.editOff();
+            $scope.removeOnId = note.id;
+            $scope.newNote = note;
+        }
+        $scope.removeOff = function () {
+            $scope.removeOnId = null;
+            $scope.newNote = null;
+        }
+        $scope.checkRemove = function (note) {
+            return $scope.removeOnId === note.id;
+        }
+
+        $scope.checkActions = function (note) {
+            return $scope.checkEdit(note) || $scope.checkRemove(note);
+        }
+
+        notesService.getNotes();
         
     }
     ]));
